5eee4e217b884948e2de6db5b0b37cf05a23c359
Zer0CoolX <36512355+Zer0CoolX@users.noreply.github.com>
2019-03-20 16:44:50 -0400

    Update guac-install.sh
    Reverted default for MYSQL_CON_VER to 5.1.47 from 8.0.15 as 8x was causing issues with mariaDB that resulted in blank login screen (see issue #38. Feel free to try 8.x but verify it works before relying on it. 8.x seems to work for me with a Lets Encrypt SSL cert and public domain, but not in testing with a self signed cert and local access via IP/hostname.

--- guac-install.sh
+++ guac-install.sh
@@ -26,7 +26,7 @@ ADM_POC="Local Admin, admin@admin.com"  # Point of contact for the Guac server a
 
 # Versions
 GUAC_STBL_VER="1.0.0" # Latest stable version of Guac from https://guacamole.apache.org/releases/
-MYSQL_CON_VER="8.0.15" # Latest stable release of MySQL Connecter J
+MYSQL_CON_VER="5.1.47" # Working stable release of MySQL Connecter J
 LIBJPEG_VER="2.0.2" # Latest stable version of libjpeg-turbo
 MAVEN_VER="3.6.0" # Latest stable version of Apache Maven
 
