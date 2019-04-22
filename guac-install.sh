59ffb09e6bc435d890a67b0e153532afe6c44c8d
Zer0CoolX <36512355+Zer0CoolX@users.noreply.github.com>
2019-04-22 11:56:51 -0400

    Update guac-install.sh
    Resolve issue with mvn command not found error. Was in a sub process so it didnt apply the export of the env variable.

--- guac-install.sh
+++ guac-install.sh
@@ -24,7 +24,7 @@ set -E
 ######  UNIVERSAL VARIABLES  #########################################
 # USER CONFIGURABLE #
 # Generic
-SCRIPT_BUILD="2019_4_16" # Scripts Date for last modified as "yyyy_mm_dd"
+SCRIPT_BUILD="2019_4_22" # Scripts Date for last modified as "yyyy_mm_dd"
 ADM_POC="Local Admin, admin@admin.com"  # Point of contact for the Guac server admin
 
 # Versions
@@ -1108,10 +1108,10 @@ if [ $GUAC_SOURCE == "Git" ]; then
 		wget ${MAVEN_URL}${MAVEN_BIN}
 		tar -xvzf ${MAVEN_BIN}
 		ln -s ${MAVEN_FN} maven
-		export PATH=/opt/maven/bin:${PATH}
 		rm -rf /opt/${MAVEN_BIN}
 	} &
 	s_echo "n" "-Installing Apache Maven for git...    "; spinner
+	export PATH=/opt/maven/bin:${PATH}
 	cd ~
 fi
 
