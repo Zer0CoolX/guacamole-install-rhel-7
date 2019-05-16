292a9f561930924ff7cf5a23c8638a1242ff7906
Zer0CoolX <36512355+Zer0CoolX@users.noreply.github.com>
2019-05-16 11:21:43 -0400

    Update guac-install.sh
    removed unsafe-inline from script-src for CSP.

--- guac-install.sh
+++ guac-install.sh
@@ -1462,7 +1462,7 @@ s_echo "n" "${Reset}-Generate Nginx guacamole.config...    "; spinner
 		ssl_session_tickets off;
 		add_header Referrer-Policy \"no-referrer\";
 		add_header Strict-Transport-Security \"max-age=15768000; includeSubDomains\" always;
-		add_header Content-Security-Policy \"default-src 'none'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; connect-src 'self'; object-src 'self'; frame-src 'self'; img-src 'self' data:; style-src 'self' 'unsafe-inline'; font-src 'self'; form-action 'self'; base-uri 'self'; frame-ancestors 'self';\" always;
+		add_header Content-Security-Policy \"default-src 'none'; script-src 'self' 'unsafe-eval'; connect-src 'self'; object-src 'self'; frame-src 'self'; img-src 'self' data:; style-src 'self' 'unsafe-inline'; font-src 'self'; form-action 'self'; base-uri 'self'; frame-ancestors 'self';\" always;
 		add_header X-Frame-Options \"SAMEORIGIN\" always;
 		add_header X-Content-Type-Options \"nosniff\" always;
 		add_header X-XSS-Protection \"1; mode=block\" always;
